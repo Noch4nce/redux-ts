3afa5efd2bdd5c799b90302e50668c9b17db6879
Noch4nce <m4rgera123@mail.ru>
2022-09-26 00:39:04 +0500

    todo fetch

---
+++ src/action-creators/fetchTodos.ts
@@ -0,0 +1,33 @@
+import { FieldTodoActionTypes, TodoActionTypes } from '../types/todoTypes'
+import { Dispatch } from 'redux'
+import axios from 'axios'
+
+export const fetchTodos = (page = 1, limit = 10) => {
+	return async (dispatch: Dispatch<FieldTodoActionTypes>) => {
+		try {
+			dispatch({ type: TodoActionTypes.FETCH_ACTION })
+			const response = await axios.get(
+				`https://jsonplaceholder.typicode.com/todos`,
+				{
+					params: {
+						_page: page,
+						_limit: limit
+					}
+				}
+			)
+			dispatch({
+				type: TodoActionTypes.FETCH_ACTION_SUCCESS,
+				payload: response.data
+			})
+		} catch (e) {
+			dispatch({
+				type: TodoActionTypes.FETCH_ACTION_ERROR,
+				payload: 'Fetch todo error'
+			})
+		}
+	}
+}
+
+export const serTodoPage = (page: number): FieldTodoActionTypes => {
+	return { type: TodoActionTypes.SET_TODO_PAGE, payload: page }
+}
